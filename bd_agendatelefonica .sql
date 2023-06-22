965a7c0f57fb6a5a6d6e5780df05544f7e2c3598
Ocio75 <133545261+Ocio75@users.noreply.github.com>
2023-06-23 00:30:55 +0200

    versión final

--- bd_agendatelefonica .sql
+++ bd_agendatelefonica .sql
@@ -1,33 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 05-06-2023 a las 07:23:16
--- Versión del servidor: 8.0.31
--- Versión de PHP: 8.0.26
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `bd_agendatelefonica`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Uso de la base de datos
+USE `bd_agendatelefonica`;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `bd_agendatelefonica`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `contactos`
---
-
+-- Eliminación de tablas existentes
 DROP TABLE IF EXISTS `contactos`;
+DROP TABLE IF EXISTS `tipocontacto`;
+
+-- Creación de la tabla 'contactos'
 CREATE TABLE IF NOT EXISTS `contactos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
@@ -43,35 +24,23 @@ CREATE TABLE IF NOT EXISTS `contactos` (
   KEY `idTipocon` (`idTipocon`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Volcado de datos para la tabla `contactos`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipocontacto`
---
-
-DROP TABLE IF EXISTS `tipocontacto`;
+-- Creación de la tabla 'tipocontacto'
 CREATE TABLE IF NOT EXISTS `tipocontacto` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Volcado de datos para la tabla `tipocontacto`
---
-
+-- Inserción de datos de ejemplo en la tabla 'tipocontacto'
 INSERT INTO `tipocontacto` (`id`, `nombre`) VALUES
 (1, 'Sin etiquetar'),
 (2, 'Familia'),
 (3, 'Trabajo');
-COMMIT;
 
---usuario
-CREATE USER 'ConexionAgenda'@'%' IDENTIFIED WITH caching_sha2_password BY '***';GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, FILE, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON *.* TO 'ConexionAgenda'@'%';ALTER USER 'ConexionAgenda'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `bd\_agendatelefonica`.* TO 'ConexionAgenda'@'%';
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Creación del usuario y asignación de permisos
+CREATE USER 'ConexionAgenda'@'%' IDENTIFIED WITH caching_sha2_password BY '***';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, FILE, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON `bd_agendatelefonica`.* TO 'ConexionAgenda'@'%';
+ALTER USER 'ConexionAgenda'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+GRANT ALL PRIVILEGES ON `bd_agendatelefonica`.* TO 'ConexionAgenda'@'%';
+
+-- Fin del script SQL
